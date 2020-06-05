26033020780f2458b738d3fc7dec592ca2794984
ProfPaulB <paulbonenfant@hotmail.com>
2020-06-05 14:36:16 -0400

    Introduced enums for looser coupling

--- src/cardgame/CardGame.java
+++ src/cardgame/CardGame.java
@@ -1,5 +1,7 @@
 package cardgame;
 
+import cardgame.Card.Suit;
+import cardgame.Card.Value;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -17,46 +19,59 @@ public class CardGame {
 
         Scanner input = new Scanner(System.in);
 
-        Card[] cardHand = new Card[7];
+        Card[] cardHand = new Card[20];
         Random random = new Random();
 
         for (int i = 0; i < cardHand.length; i++) {
 
-            int value = random.nextInt(13) + 1;
+            Value value = Card.Value.values()[
+                    random.nextInt(Card.Value.values().length)];
 
-            String suit = Card.SUITS[random.nextInt(4)];
+            Suit suit = Card.Suit.values()[
+                    random.nextInt(Card.Suit.values().length)];
 
             Card card = new Card(value, suit);
 
             cardHand[i] = card;
         }
-
+       
         System.out.println("Here are the cards");
 
         for (Card card : cardHand) {
-            System.out.printf("%d of %s\n",
+            System.out.printf("%s of %s\n",
                     card.getValue(), card.getSuit());
         }
         
-        System.out.println("\nChoose a suit");
-        for (int i = 0; i < Card.SUITS.length; i++) {
+        System.out.println("\nChoose a suit:");
+        for (int i = 0; i < Card.Suit.values().length; i++) {
 
-            System.out.println((i + 1) + ": " + Card.SUITS[i]);
+            System.out.println((i + 1) + ": " + 
+                    Card.Suit.values()[i]);
 
         }
-        int suit = input.nextInt();
+        
+        int suit = input.nextInt() - 1;
 
-        System.out.println("Enter the value (1 to 13)");
-        int value = input.nextInt();
+        System.out.println("Please choose the value");
+        
+        for (int i = 0; i < Card.Value.values().length; i++) {
+            System.out.println((i + 1) + ": " +
+                    Card.Value.values()[i]);
+        
+        }
+        
+        int value = input.nextInt() - 1;
 
-        Card userGuess = new Card(value, Card.SUITS[suit - 1]);
+        Card userGuess = new Card(
+            Card.Value.values()[value],
+            Card.Suit.values()[suit]);
         
         boolean match = false;
         
         for (Card card: cardHand) {
         
             if (card.getValue() == userGuess.getValue() &&
-                    card.getSuit().equals(userGuess.getSuit())) {
+                    card.getSuit() == userGuess.getSuit()) {
             
                 match = true;
                 break;
